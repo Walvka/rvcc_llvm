c4c1d98b14106554de885f94e1ad2c566de27feb
Walvka <2411082614@qq.com>
2023-04-19 10:31:38 +0800

    [1]项目第一步，先建立一个工程，输出hello world

---
+++ include/chilang/Basic/Version.h
@@ -0,0 +1,12 @@
+#ifndef CHILANG_BASIC_VERSION_H
+#define CHILANG_BASIC_VERSION_H
+
+#include "chilang/Basic/Version.inc"
+#include <string>
+
+namespace chilang
+{
+    std::string getChilangVersion();
+}
+
+#endif
---
+++ lib/Basic/Version.cpp
@@ -0,0 +1,6 @@
+#include "chilang/Basic/Version.h"
+
+std::string chilang::getChilangVersion()
+{
+    return CHILANG_VERSION_STRING;
+}
